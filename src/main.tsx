c80c437e816632917f75e9e5aaeb278ce2c04010
Onyiee <elizabethonyemowo.agbo@gmail.com>
2024-11-25 22:05:48 +0100

    added a button

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+import "./index.css";
+
+const theme = extendTheme({});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
