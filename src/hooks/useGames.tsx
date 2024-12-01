4611b52373e003f314ab16abbf0acb634b9abd5a
Onyiee <elizabethonyemowo.agbo@gmail.com>
2024-12-02 00:44:56 +0100

    Create a generic data fetching hook

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
 export interface Platform {
   id: number;
@@ -7,7 +6,7 @@ export interface Platform {
   slug: string;
 }
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
   background_image: string;
@@ -15,32 +14,6 @@ interface Game {
   metacritic: number;
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        setIsLoading(true);
-        const response = await apiClient.get<FetchGamesResponse>("/games");
-        setGames(response.data.results);
-      } catch (err: any) {
-        setError(err.message || "Failed to fetch games");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchGames();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
