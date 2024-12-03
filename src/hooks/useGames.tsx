792f3318e2196c124435443454b28ffd97374ca8
Onyiee <elizabethonyemowo.agbo@gmail.com>
2024-12-04 00:01:09 +0100

    Filter games by genre

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -14,6 +15,11 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = () => useData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>(
+    "/games",
+    selectedGenre ? { params: { genres: selectedGenre.id } } : {}, // Exclude params if no genre is selected
+    [selectedGenre?.id]
+  );
 
 export default useGames;
